417a9faf77e2108a05c8faa388b741ab6caba424
MarKsu <72389013+Marksu-u@users.noreply.github.com>
2024-09-23 18:35:25 +0200

    Add GenderController and GenderService classes
    - Added GenderController class to handle gender-related requests
    - Added GenderService class to retrieve gender information from an external API

---
+++ server/src/main/java/com/swevos_marksu/whatever_api/controller/GenderController.java
@@ -0,0 +1,23 @@
+package com.swevos_marksu.whatever_api.controller;
+
+import com.swevos_marksu.whatever_api.service.GenderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GenderController {
+
+    private final GenderService genderService;
+
+    @Autowired
+    public GenderController(GenderService genderService) {
+        this.genderService = genderService;
+    }
+
+    @GetMapping("/gender")
+    public String getGender(@RequestParam String name) {
+        return genderService.getGender(name);
+    }
+}
