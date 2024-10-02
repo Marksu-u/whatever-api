cb68351eb3fd38d55fe1418246fe3361b536ab21
MarKsu <72389013+Marksu-u@users.noreply.github.com>
2024-10-02 16:53:22 +0200

    Feature/apidog (#7)
    * Add DogController and DogService classes
    
    - Added `DogController` class to handle requests related to dogs.
    - Added `DogService` class to fetch dog images from an external API.
    
    * Changed GenderController endpoints
    
    - All endpoints in the GenderController will now have `/api` as a prefix
    
    Users can now access GenderController endpoints with `/api` prefix.

--- server/src/main/java/com/swevos_marksu/whatever_api/controller/GenderController.java
+++ server/src/main/java/com/swevos_marksu/whatever_api/controller/GenderController.java
@@ -3,10 +3,12 @@ package com.swevos_marksu.whatever_api.controller;
 import com.swevos_marksu.whatever_api.service.GenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class GenderController {
 
     private final GenderService genderService;
